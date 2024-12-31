08616b9ac632d52392018aac309cbe0b1c5da0fa
suniaikin <class.suniaikin@gmail.com>
2024-12-30 23:54:13 -0800

    added solution

---
+++ 8kyu/multiply.js
@@ -0,0 +1,16 @@
+//🎃DESCRIPTION
+//This code does not execute properly. Try to figure out why.
+
+//✅SOLUTION
+
+function multiply(a, b){
+    return a * b;
+}
+
+let result = multiply(11, 11);
+console.log(result)
+
+//💖BEST PRACTICE💖
+
+
+    
