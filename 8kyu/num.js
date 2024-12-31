4f78d3158fd28ad4b9fe84d8dcabefc1602e2a0d
suniaikin <class.suniaikin@gmail.com>
2024-12-30 17:02:31 -0800

    init

---
+++ 8kyu/num.js
@@ -0,0 +1,22 @@
+//🎃DESCRIPTION
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+//✅SOLUTION
+function makeNegative(num) {
+    if (num > 0) {
+        num = num * (-1);
+    } else if (num < 0) {
+        num = num;
+    } else {
+        num = 0;
+    }
+    return num
+}
+
+console.log(makeNegative(-1))
+
+//💖BEST PRACTICE
+
+// function makeNegative(num) {
+//     return -Math.abs(num);
+// }
