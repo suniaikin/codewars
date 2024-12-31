9a9487dff94541e3d7ee126bc873eef93edde0f7
suniaikin <class.suniaikin@gmail.com>
2024-12-30 17:07:12 -0800

    first commit

--- 8kyu/num.js
+++ 8kyu/num.js
@@ -6,7 +6,7 @@ function makeNegative(num) {
     if (num > 0) {
         num = num * (-1);
     } else if (num < 0) {
-        num = num;
+        num;
     } else {
         num = 0;
     }
@@ -15,7 +15,7 @@ function makeNegative(num) {
 
 console.log(makeNegative(-1))
 
-//💖BEST PRACTICE
+//💖BEST PRACTICE💖
 
 // function makeNegative(num) {
 //     return -Math.abs(num);
