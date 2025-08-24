07924c29455946f1c2819b6e883858ffd611d6b1
suniaikin <class.suniaikin@gmail.com>
2025-08-24 08:07:39 -0700

    function

--- sum-positive.js
+++ sum-positive.js
@@ -8,12 +8,13 @@ the sum of all of the positives ones.*/
 function positiveSum(arr) {
 	let sum = 0;
 	for (let i = 0; i < arr.length; i++) {
-	
+	if (arr[i] > 0) {
+			sum += arr[i];
+		}
 	} return sum
 
 }
 
-console.log(positiveSum([2, -2, 4]));
 
 // 💖BEST PRACTICE💖
 
