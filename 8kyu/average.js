9cc82a7e647d824f0fd052e3cf98181fb91f7fac
suniaikin <class.suniaikin@gmail.com>
2025-08-23 17:39:07 -0700

    task

--- 8kyu/average.js
+++ 8kyu/average.js
@@ -1,3 +1,22 @@
 // 🎃DESCRIPTION🎃
+/* Write a function which calculates the average
+of the numbers in a given array.
+Note: Empty arrays should return 0.
+*/
+
 //✅SOLUTION✅
+function findAverage(array) {
+	if (array.length === 0) {
+		return 0;
+	}
+	let sum = 0
+	for (let i = 0; i < array.length; i++) {
+		sum += array[i]
+	}
+	return sum / array.length
+
+}
+
+console.log(findAverage([1, 2, 3]))
+
 // 💖BEST PRACTICE💖
