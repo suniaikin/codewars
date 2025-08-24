a07ce83b3f61b1f1be728fd6e73f55b92fc9e6ec
suniaikin <class.suniaikin@gmail.com>
2025-08-23 23:44:05 -0700

    task

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,15 @@
+// 🎃DESCRIPTION🎃
+/*
+Create a function that takes an integer
+as an argument and returns "Even" for even numbers
+or "Odd" for odd numbers.
+*/
+
+//✅SOLUTION✅
+function evenOrOdd(number) {
+	return number % 2 === 0 ? "Even" : "Odd";
+}
+
+
+
+// 💖BEST PRACTICE💖
