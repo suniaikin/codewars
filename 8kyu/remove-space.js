7a62e6fb3d27e2432df9633a04a6154ea3e2a153
suniaikin <class.suniaikin@gmail.com>
2025-08-25 15:50:39 -0700

    BEST PRACTICE

--- 8kyu/remove-space.js
+++ 8kyu/remove-space.js
@@ -4,7 +4,29 @@
 
 //✅SOLUTION✅
 
+function noSpace(x) {
+	let newSpace = ""
+	for (let i = 0; i < x.length; i++) {
+		if (x[i] === " ") {
+			newSpace += ""
+		} else {
+			newSpace += x[i]
+		}
+	} return newSpace
+}
 
-
+let space = "d 2 3 K dj H"
+console.log(noSpace(space))
 
 // 💖BEST PRACTICE💖
+
+function noSpace2(x) {
+	return x.replaceAll(" ", "")
+
+}
+
+let space2 = "d 2 3 K dj H"
+console.log(noSpace2(space2))
+
+
+
