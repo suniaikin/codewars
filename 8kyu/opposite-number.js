29af55d2adbb3b61036dbcf8c61d3ee78f8b50d6
suniaikin <class.suniaikin@gmail.com>
2025-08-25 09:55:13 -0700

    task init

---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,21 @@
+// 🎃DESCRIPTION🎃
+/*
+Very simple, given a number (integer / decimal / both depending on the language),
+find its opposite (additive inverse).
+*/
+
+
+
+//✅SOLUTION✅
+
+function opposite(number) {
+
+}
+
+
+let array = [1,2,3];
+let out = positiveSum(array)
+console.log(out)
+
+
+// 💖BEST PRACTICE💖
