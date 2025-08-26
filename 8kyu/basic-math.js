35e84712e0bf76575bd8f3b51acc027945187f11
suniaikin <class.suniaikin@gmail.com>
2025-08-25 22:18:26 -0700

    traine init

---
+++ 8kyu/basic-math.js
@@ -0,0 +1,15 @@
+// 🎃DESCRIPTION🎃
+
+/*
+Your task is to create a function that does four basic mathematical operations.
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+ */
+
+//✅SOLUTION✅
+
+function basicOp(operation, value1, value2){
+
+}
+
+// 💖BEST PRACTICE💖
