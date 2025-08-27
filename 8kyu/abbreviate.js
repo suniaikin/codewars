20ce0d8f0d9ebb62575c036dfd03568fa4bea0ec
suniaikin <class.suniaikin@gmail.com>
2025-08-26 23:53:17 -0700

    SOLUTION

--- 8kyu/abbreviate.js
+++ 8kyu/abbreviate.js
@@ -7,5 +7,12 @@ The output should be two capital letters with a dot separating them.
 
 //✅SOLUTION✅
 
+let nameString = "sarah connor"
+let str = ""
 
+function abbrevName(name) {
+	return name[0].toUpperCase()+ "." + name[name.indexOf(" ") + 1].toUpperCase()
+}
+
+console.log(abbrevName(nameString))
 // 💖BEST PRACTICE💖
