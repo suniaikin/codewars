868de4110db5d79cd2d2e84223166f6ce696ca90
suniaikin <class.suniaikin@gmail.com>
2025-08-26 22:08:10 -0700

    kata init

---
+++ 8kyu/abbreviate.js
@@ -0,0 +1,11 @@
+// 🎃DESCRIPTION🎃
+/*
+Write a function to convert a name into initials.
+This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+ */
+
+//✅SOLUTION✅
+
+
+// 💖BEST PRACTICE💖
