f01cfffd58a16bf7eafc3ecd0bd262462e6907cb
suniaikin <class.suniaikin@gmail.com>
2025-09-03 20:55:13 -0700

    task init

---
+++ 8kyu/array-to-object.js
@@ -0,0 +1,12 @@
+const data = [
+    ['name', 'Сергей'],
+    ['city', 'Москва'],
+    ['isMarried', true]
+];
+
+// Ваш код здесь
+// ...
+
+console.log(dataObject);
+// Ожидаемый результат:
+// { name: 'Сергей', city: 'Москва', isMarried: true }
