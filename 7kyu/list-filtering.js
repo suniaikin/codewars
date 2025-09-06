1cefbed5e7de111ce53134d37b8bb4020baeb6ee
suniaikin <class.suniaikin@gmail.com>
2025-09-06 16:05:43 -0700

    task solving

--- 7kyu/list-filtering.js
+++ 7kyu/list-filtering.js
@@ -1,3 +1,9 @@
 /*
 In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 */
+
+const arr = [1, 2, 3, 'a', 'b']
+
+const getIntegers = (arr) => arr.filter(item => Number.isInteger(item))
+
+console.log(getIntegers(arr)) // [1, 2, 3]
